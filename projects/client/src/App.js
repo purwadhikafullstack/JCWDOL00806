81c4ad5481a2727e3ea4eaca269bfa1f5fbb43d6
BryanTian28 <austinbryan2000@gmail.com>
2023-03-09 21:12:57 +0700

    Tenant Property Category CRUD

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -2,6 +2,7 @@ import TenantRegister from "./pages/tenantRegister";
 import TenantLogin from "./pages/tenantLogin";
 import { Routes, Route } from "react-router-dom";
 import TenantVerify from "./pages/tenantVerify";
+import TenantDashboard from "./pages/tenantDashboard";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/tenantRegister" element={<TenantRegister />} />
         <Route path="/tenantLogin" element={<TenantLogin />} />
         <Route path="/tenantVerify/:username" element={<TenantVerify />} />
+        <Route path="/tenantDashboard" element={<TenantDashboard />} />
       </Routes>
     </>
   );
--- projects/server/src/controllers/tenantController.js
+++ projects/server/src/controllers/tenantController.js
@@ -7,10 +7,12 @@ const fs = require("fs").promises;
 //Import models
 const db = require("../../models/index");
 const tenant = db.tenant;
+const propertyCategory = db.property_category;
 
 //Import hash & jwt
 const { hashPassword, hashMatch } = require("../lib/hash");
 const { createToken } = require("../lib/jwt");
+const { get } = require("http");
 
 module.exports = {
   register: async (req, res) => {
@@ -104,6 +106,124 @@ module.exports = {
         data: {
           token: createToken({ id: findUsernameOrEmail.dataValues.id }),
         },
+      });
+    } catch (error) {}
+  },
+
+  getCategory: async (req, res) => {
+    try {
+      let { username } = req.query;
+      let getTenantId = await tenant.findOne({
+        attributes: ["id"],
+        where: { username: username },
+      });
+      let getData = await propertyCategory.findAll({
+        where: {
+          tenant_id: getTenantId.dataValues.id,
+        },
+      });
+      let data = [];
+      getData.forEach((value) => {
+        data.push(value.dataValues);
+      });
+      res.status(201).send({
+        isError: false,
+        message: "Data Acquired",
+        data,
+      });
+    } catch (error) {}
+  },
+
+  createCategory: async (req, res) => {
+    const t = await sequelize.transaction();
+    try {
+      let { type, city } = req.body;
+      let { username } = req.query;
+      let getTenantId = await tenant.findOne({
+        attributes: ["id"],
+        where: { username: username },
+      });
+      let findExisting = await propertyCategory.findOne({
+        where: {
+          [Op.and]: {
+            type,
+            city,
+            tenant_id: getTenantId.dataValues.id,
+          },
+        },
+      });
+      if (findExisting !== null) {
+        res.status(500).send({
+          isError: false,
+          message: "Type and City combination already exists",
+          data: null,
+        });
+      } else {
+        await propertyCategory.create({
+          type,
+          city,
+          tenant_id: getTenantId.dataValues.id,
+        });
+        res.status(200).send({
+          isError: false,
+          message: "Created Successfully",
+          data: null,
+        });
+        t.commit();
+      }
+    } catch (error) {
+      t.rollback();
+      console.log(error);
+    }
+  },
+  updateCategory: async (req, res) => {
+    const t = await sequelize.transaction();
+    try {
+      let { newType, newCity } = req.body;
+      let { type, city, username } = req.query;
+      let getTenantId = await tenant.findOne({
+        attributes: ["id"],
+        where: { username: username },
+      });
+      let findExisting = await propertyCategory.update(
+        {
+          type: newType,
+          city: newCity,
+        },
+        {
+          where: {
+            [Op.and]: {
+              type,
+              city,
+              tenant_id: getTenantId.dataValues.id,
+            },
+          },
+        }
+      );
+      res.status(200).send({
+        isError: false,
+        message: "Updated Successfully",
+        data: null,
+      });
+      t.commit();
+    } catch (error) {
+      t.rollback();
+      console.log(error);
+    }
+  },
+
+  deleteCategory: async (req, res) => {
+    try {
+      let { id } = req.query;
+      await propertyCategory.destroy({
+        where: {
+          id,
+        },
+      });
+      res.status(201).send({
+        isError: false,
+        message: "Deleted Successfully",
+        data: null,
       });
     } catch (error) {}
   },
