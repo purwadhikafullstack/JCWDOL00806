e552913bf353241863e69377365ed4d0834715a9
Abdi Alghifara Felinanda <52093497+Abdi-01@users.noreply.github.com>
2023-05-10 21:32:31 +0700

    Update config.js

--- projects/server/src/config/config.js
+++ projects/server/src/config/config.js
@@ -18,10 +18,10 @@ module.exports = {
     "timezone" : "+07:00"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql",
     "timezone" : "+07:00"
   }
