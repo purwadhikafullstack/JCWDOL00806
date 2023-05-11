e301aa99a5e5e5d1ca78c273124ff44ddef97a73
Abdi Alghifara Felinanda <52093497+Abdi-01@users.noreply.github.com>
2023-05-11 19:18:17 +0700

    Update authGoogleRouters.js

--- projects/server/src/routes/authGoogleRouters.js
+++ projects/server/src/routes/authGoogleRouters.js
@@ -6,13 +6,13 @@ const { createToken } = require('../lib/jwt')
 Router.get('/', passport.authenticate("google", { scope: ["profile", "email"] }))
 Router.get('/callback', passport.authenticate("google", {
     // failureMessage: 'Cannot login to Google, please try again later!',
-    failureRedirect: 'http://localhost:3000/users/register?error=true',
+    failureRedirect: `${process.env.WEBSITE_URL}/users/register?error=true`,
     // failureFlash: true
 }),
     function (req, res) {
         const token = createToken({id: req.user.id})
         res.cookie('jwt', token)
-        res.redirect('http://localhost:3000/passport-login')
+        res.redirect(`${process.env.WEBSITE_URL}/passport-login`)
     }
 )
 
